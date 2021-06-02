4e1ec37a2874067faec6b3a698d1825a46d15473
Kitine <christine.quach.2022@ensta-paris.fr>
2021-06-02 18:35:05 +0200

    pour les vecteurs

--- harmo_sphe.py
+++ harmo_sphe.py
@@ -2,14 +2,14 @@ import numpy as np
 import math 
 
 def legendre(l,x,indic):
-	P=np.zeros((l+1,2*l+1)) 
-	P[0,0]=1
+	P=np.zeros((l+1,2*l+1),dtype=object) 
+	P[0,0]=np.ones(x.shape)
 	if (l>=1):
-		P[1,0],P[1,1]=x,-math.sqrt(1-x**2)
+		P[1,0],P[1,1]=x,-np.sqrt(1-x**2)
 		P[1,-1]=-P[1,1]/2
 	if (l>=2):
 		for L in range (2,l+1):
-			P[L,L]=-(2*L-1)*math.sqrt(1-x**2)*P[L-1,L-1]
+			P[L,L]=-(2*L-1)*np.sqrt(1-x**2)*P[L-1,L-1]
 			P[L,0]=((2*L-1)/L)*x*P[L-1,0]-(L-1)*P[L-2,0]/L
 			P[L,-L]=((-1)**L)*math.factorial(L-L)/math.factorial(L+L)*P[L,L]
 			for m in range(1,L):
@@ -22,20 +22,21 @@ def legendre(l,x,indic):
 
 
 def harmonique(l,theta,indic):
-	Y=legendre(l,math.cos(theta),indic)
-	Y[0,0]=math.sqrt(1/(4*math.pi))*Y[0,0]
+	Y=legendre(l,np.cos(theta),indic)
+	print(Y.shape)
+	Y[0,0]=np.sqrt(1/(4*math.pi))*Y[0,0]
 	if (l>=1):
-		Y[1,0]=math.sqrt((2*1+1)/(4*math.pi))*Y[1,0]
-		Y[1,1]=math.sqrt((2*1+1)/(4*math.pi*math.factorial(2*1)))*Y[1,1]
-		Y[1,-1]=math.sqrt((2*1+1)*math.factorial(1+1)/(4*math.pi*math.factorial(1-1)))*Y[1,-1]
+		Y[1,0]=np.sqrt((2*1+1)/(4*math.pi))*Y[1,0]
+		Y[1,1]=np.sqrt((2*1+1)/(4*math.pi*math.factorial(2*1)))*Y[1,1]
+		Y[1,-1]=np.sqrt((2*1+1)*math.factorial(1+1)/(4*math.pi*math.factorial(1-1)))*Y[1,-1]
 	if (l>=2):
 		for L in range (2,l+1):
-			Y[L,L]=math.sqrt((2*L+1)/(4*math.pi*math.factorial(2*L)))*Y[L,L]
-			Y[L,0]=math.sqrt((2*L+1)/(4*math.pi))*Y[L,0]
-			Y[L,-L]=math.sqrt((2*L+1)*math.factorial(L+L)/(4*math.pi))*Y[L,-L]
+			Y[L,L]=np.sqrt((2*L+1)/(4*math.pi*math.factorial(2*L)))*Y[L,L]
+			Y[L,0]=np.sqrt((2*L+1)/(4*math.pi))*Y[L,0]
+			Y[L,-L]=np.sqrt((2*L+1)*math.factorial(L+L)/(4*math.pi))*Y[L,-L]
 			for m in range(1,L):
-				pos=math.sqrt((2*L+1)*math.factorial(L-m)/(4*math.pi*math.factorial(L+m)))
-				neg=math.sqrt((2*L+1)*math.factorial(L+m)/(4*math.pi*math.factorial(L-m)))
+				pos=np.sqrt((2*L+1)*math.factorial(L-m)/(4*math.pi*math.factorial(L+m)))
+				neg=np.sqrt((2*L+1)*math.factorial(L+m)/(4*math.pi*math.factorial(L-m)))
 				Y[L,m]=pos*Y[L,m]
 				Y[L,-m]=neg*Y[L,-m]
 	if (indic=='tab'):
--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import time
 Paramètres
 """
 m=3
-l=4
-h=0.1
+l=10
+h=0.001
 x=np.arange(0,math.pi+h,h)
 
 """
@@ -29,7 +29,7 @@ for k in x:
 #Python 
 y2=scipy.special.sph_harm(m, l, 0, x)
 
-hihi=hs.harmonique(l,x,3)
+hihi=hs.harmonique(l,x,'tab')
 print(hihi)
 
 """
@@ -51,16 +51,20 @@ Comparaison du temps
 x0=math.pi/2
 #Ma fonction
 ti1=time.time()
-Y=hs.harmonique(l,x0,'tab')
+Y=hs.harmonique(l,x,'tab')
 tf1=time.time()-ti1
+
 #Celle de python 
+
 ti2=time.time()
-Ypyt=np.zeros((l+1,2*l+1))
+Ypyt=np.zeros((l+1,2*l+1),dtype=object)
 for L in range (0,l+1):
 	for m in range(0,L+1):
-		Ypyt[L,m]=scipy.special.sph_harm(m, L ,0, x0)
-		Ypyt[L,-m]=scipy.special.sph_harm(-m, L ,0, x0)
+		Ypyt[L,m]=scipy.special.sph_harm(m, L ,0, x)
+		Ypyt[L,-m]=scipy.special.sph_harm(-m, L ,0, x)
 tf2=time.time()-ti2
 
+print(tf1)
+print(tf2)
 #je suis plus rapide hehee efin sauf pour les grands l D:
 
